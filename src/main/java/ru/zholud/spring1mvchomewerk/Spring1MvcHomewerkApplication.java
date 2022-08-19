bad86722eb73b48b5eeb61429b6c4cf302fff84d
ZholudV <goldexzholud@gmail.com>
2022-08-19 13:30:11 +0500

    первый комит

---
+++ src/main/java/ru/zholud/spring1mvchomewerk/Spring1MvcHomewerkApplication.java
@@ -0,0 +1,13 @@
+package ru.zholud.spring1mvchomewerk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1MvcHomewerkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring1MvcHomewerkApplication.class, args);
+	}
+
+}
